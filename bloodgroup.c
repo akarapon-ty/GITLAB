029cb201e8751bfe679e63168fb02ff4b9700870
beraba <akarapon.ty@hotmail.com>
2019-11-19 23:45:28 +0700

    test

--- bloodgroup.c
+++ bloodgroup.c
@@ -20,7 +20,7 @@ void main()
     }  while (strcmp(G, "A") !=0  &&
                             strcmp(G, "B") !=0  &&
                             strcmp(G, "AB") !=0   &&
-                            strcmp(G, "O") !=0 );
+                            strcmp(G, "test") !=0 );
     if (strcmp(G,"A") ==0)
     {
       printf("%s, A. Hey, you can give blood to: A, AB testchange.\n", Name);
@@ -31,7 +31,7 @@ void main()
       printf("%s, B. Well, you can give blood to: B, AB.\n", Name);
       printf("  You can receive blood from: B, O.\n");
     }
-    else if (strcmp(G, "AB") == 0)
+    else if (strcmp(G, "ABCD") == 0)
     {
       printf("%s, AB. Oh my God, you can give blood only to: AB.\n", Name);
       printf("  Wow, you can receive blood from all: O, A, B, AB.\n");
